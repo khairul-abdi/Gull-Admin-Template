80f014b48c95b809b9fed100668a2a36c5575c4e
Matthew Troost <matthew.troost@hotmail.com>
2019-10-15 11:51:26 +0200

    Initial commit

---
+++ src/views/app/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- <keep-alive> -->
+    <component is="compact-sidebar"></component>
+    <!-- </keep-alive> -->
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters(["getThemeMode"])
+  },
+  methods: {}
+};
+</script>
